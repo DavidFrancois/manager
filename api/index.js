090111e47252e5732e37714a7245c96a4a16243b
Francois David <francois.david@diji.fr>
2020-04-13 01:26:46 +0200

    [NODE] Log host when starting serv

--- api/index.js
+++ api/index.js
@@ -4,5 +4,6 @@ const App = require('./app');
 const http = require('http').Server(App);
 
 const port = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
 
-http.listen(port, () => console.log('Server listening on port: ' + port));
+http.listen(port, HOST, () => console.log(`Server listening on http://${HOST}/${port}`));
